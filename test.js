1b9193ca4fa0503cba1e76a78dc63d668ddd8cce
Guillaume Grossetie <ggrossetie@gmail.com>
2018-12-12 00:03:19 +0100

    Remove launch options

--- test.js
+++ test.js
@@ -1,16 +1,9 @@
 /* eslint-env node, es6 */
 const puppeteer = require('puppeteer-firefox')
 
-// puppeteer options
-const opts = {
-  headless: true,
-  timeout: 10000,
-  args: ['--allow-file-access-from-files', '--no-sandbox']
-};
-
 (async function () {
   try {
-    const browser = await puppeteer.launch(opts);
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     browser.on('error', (err) => console.log('err', err));
     page.on('error', (err) => console.log('err', err));
