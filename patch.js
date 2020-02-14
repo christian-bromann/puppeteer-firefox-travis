17a5078a876afc5fe15d55bca720f9abddfdd0ff
christian-bromann <mail@christian-bromann.com>
2020-02-14 16:01:25 +0100

    node 10

--- patch.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-
-// monkey patch
-let content = fs.readFileSync('VerboseLauncher.js', 'utf-8')
-fs.writeFileSync('node_modules/puppeteer-firefox/lib/firefox/Launcher.js', content, 'utf-8')
